e8abf6cec78d7cc1f8149927d5db9d6c93f8eece
Gabe M <gmarkv10@gmail.com>
2015-11-15 22:09:34 -0500

    pre-import

--- LyricAI/src/LyricAIDriver.java
+++ LyricAI/src/LyricAIDriver.java
@@ -1,15 +1,42 @@
+import java.io.File;
 import java.io.IOException;
 
 public class LyricAIDriver {
 
 	public static void main(String[] args) {
 		// TODO Get There
-		FrequencyProcessor fp = new FrequencyProcessor("hello",3);
-		fp.processLines();
+		
+		FrequencyProcessor fp;
+		try {
+			File lyrdir = new File((new File(".").getCanonicalPath()) + "\\Lyrics");
+			String[] lyrfiles = lyrdir.list();
+			for(int  i = 0; i < lyrfiles.length; i++){
+				System.out.println("PROCESSING: " + lyrfiles[i]);
+				for(int phraseLength = 1; phraseLength <= 3; phraseLength++){
+					if(phraseLength == 1){
+						fp = new FrequencyProcessor(lyrfiles[i]);
+					}
+					else{
+						fp = new FrequencyProcessor(lyrfiles[i], phraseLength);
+					}
+					fp.processLines();
+					fp.exportJSON();
+					fp.close();
+					fp = null;
+				}
+				
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//FrequencyProcessor fp = new FrequencyProcessor("hello");
+		//fp.processLines();
 		
 		//System.out.println(fp.frqMap.toJSON(1));
 		
-		fp.exportJSON();
+		//fp.exportJSON();
 	
 /*		MapSetList m = new MapSetList<String>();
 		
