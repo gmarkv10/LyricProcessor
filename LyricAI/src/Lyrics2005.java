281ddd9244cb4cc1910971cd67b0cec1068e7af2
Gabe M <gmarkv10@gmail.com>
2015-11-22 21:07:43 -0500

    Final Lyrics class - lyrics2005 added

--- LyricAI/src/Lyrics2005.java
+++ LyricAI/src/Lyrics2005.java
@@ -5,17 +5,17 @@ public class Lyrics2005 {
 		
 		//query the db for [Songname, artist] from 'lyrics' pair and store in an array
 		
-		//for([Songname, artist] in ):
+		//for([Songname, artist] in that object):
 		//	song = songname
 		// 	art = artist
-		//  week = do the same thing for week
+		//  week = select [Songname, artist] order anscending, take the first
 		//	lyric = query the db by [Songname, artist] for lyrics store in a string
 		//  top10[] = frequencyProcessor.read(lyric);
 		//    ... and so on for 2 and 3
 		//  speech[] = POSProcesor.read(lyric)
 		//  distinct = frequencyprocessor.length
 		//  total/distinct; pretty self explantatory
-		//		rank = select [Songname, artist] order anscending, take the first
+		//	rank = select [Songname, artist] order anscending, take the first
 		
 		//contruct a line for an aarf file
 		//write that line to an aarf file
