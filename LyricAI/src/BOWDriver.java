91bab17b40bdf196bd1d6cd7eafd7aa6263425b4
Gabe M <gmarkv10@gmail.com>
2015-12-09 09:58:56 -0500

    tail end of configuring BagOfWords

---
+++ LyricAI/src/BOWDriver.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+public class BOWDriver {
+	
+	static HashMap map = new HashMap<String, Integer>();
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+
+		String lyric = "you used to call me on my you used to you used to";
+		String[] words = lyric.split(" ");
+		for(String 	w : words){
+			Integer freq =  (Integer) map.get(w);
+			map.put(w,freq == null? 1 : ++freq);
+		}
+		
+		
+		Set<String> set =  new TreeSet<String>();
+		set.add("you");
+		set.add("used");
+		set.add("to");
+		set.add("call");
+		set.add("me");
+		set.add("on");
+		set.add("my");
+		Iterator<String> ite = set.iterator();
+		while(ite.hasNext()){
+			String s = ite.next();
+			System.out.println(s + " - " + map.get(s));
+		}
+		
+//		BagOfWords b =  new BagOfWords("test");
+//		b.writeaarfheader();
+//		b.writer.close();
+
+	}
+	
+	
+
+}
---
+++ LyricAI/src/BagOfWords.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+
+public class BagOfWords{
+	HashMap<String, Integer> map = new HashMap<String, Integer>();
+	Set<String> sortedKeys =  new TreeSet<String>();
+	String line = null;
+	
+	BufferedReader reader; 
+	BufferedWriter writer;
+	String name;
+	
+	queries q;
+	String[][] SONGINFO;
+	String lyric;
+
+	
+	public BagOfWords(String n) throws Exception{
+		// TODO Auto-generated method stub
+		name = n;
+		reader =  new BufferedReader(new FileReader("Data/globalFreq_StdDev.csv"));
+		writer =  new BufferedWriter(new FileWriter("Data/bigheader.arff"));
+		String[] data = null; String word, freq;
+		//constructing map
+		System.out.println("constructing map");
+		while((line =reader.readLine()) != null){
+			data = line.split(",");
+			word = data[0];
+			map.put(word, 0);
+			sortedKeys.add(word);
+		}
+		System.out.println("making query");
+		q = new queries();
+		SONGINFO = q.songANDartistANDbestweekANDbestrank();
+		System.out.println("done");
+	}
+	
+	void resetMap(){
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+		    entry.setValue(0);
+		}
+	}
+	
+	public void playSong(int i) throws ClassNotFoundException, IOException{
+		lyric = q.getLyrics(SONGINFO[i][0], SONGINFO[i][1]);
+		String[] words = lyric.split(" ");
+		for(String 	w : words){
+			Integer freq =  map.get(w);
+			map.put(w,++freq);
+		}
+		Iterator it = sortedKeys.iterator();
+		while(it.hasNext()){
+			writer.write(map.get(it.next()) +",");
+		//	writer.write();
+		}
+		
+	}
+	
+	
+	
+	void writeaarfheader() throws IOException{
+		if(writer == null){
+			System.out.print("run init.");
+		}
+		else{
+			
+			writer.write("@relation " + name); writer.newLine();
+			writer.newLine();
+			int i = 1;
+			Iterator it = map.keySet().iterator();
+			
+			while(it.hasNext()){
+				writer.write("@attribute " + it.next() + i++); writer.newLine();
+			}
+			
+			writer.write("@attribute timespan {early1980s,mid1980s,late1980s,early1990s,mid1990s,late1990s,early2000s,mid2000s,late2000s,early2010s,mid2010s}" );
+			writer.newLine();
+			writer.newLine();
+			writer.write("@data"); writer.newLine();
+		}
+		
+		
+	}
+
+}
